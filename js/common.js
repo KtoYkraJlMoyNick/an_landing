bb719a5f6519c14f0a67734e11397701abc14c59
Никита Агупов <nktrsk@gmail.com>
2016-02-10 18:17:56 +0300

    Current

---
+++ js/common.js
@@ -0,0 +1,61 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(document).ready(function(){
+	function heightDetect(){
+		$(".main-header").css("min-height", $(window).height());
+	};
+	heightDetect();
+	$( window ).resize(function() {
+ 		 heightDetect();
+	});
+	$(".main-header").parallax(	{imageSrc: 'img/bg.jpg'});
+	$(".c-hamburger").on("click", function(){
+		$(".main-header__nav-wrap").fadeToggle("linear");
+	});
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
